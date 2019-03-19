036b5884f0b539a4e67d67f7d8de26c38e108dda
Eduardo Chavez <jose_lalo_13@hotmail.com>
2019-03-18 23:26:40 -0700

    CORS_3

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,17 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();    
 }
 
-
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
 const path = require('path');
-const cors = require('cors');
+
 
 //console.log(process.env.NODE_ENV)
 //INITIALIZATIONS
 const app = express();
+app.use(cors())
 require('./database');
 
 //SETTINGS
@@ -19,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 
 //MIDDLEWARES
-app.use(cors());
+
 app.use(morgan('dev'));
 /*
 const storage = multer.diskStorage({
@@ -29,12 +30,6 @@ const storage = multer.diskStorage({
     }
 });
 app.use(multer({storage}).single('image'));*/
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());//leer json
 
--- fronted/scripts/script.js
+++ fronted/scripts/script.js
@@ -1,7 +1,18 @@
-fetch('https://api-lindo-detalle.herokuapp.com/api/product')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
+
+
+
+var misCabeceras = new Headers();
+
+var miInit = { method: 'GET',
+               headers: misCabeceras,
+               mode: 'cors',
+               cache: 'default' };
+
+fetch('https://api-lindo-detalle.herokuapp.com/api/product',miInit)
+.then(function(response) {
+  return response.json();
+})
+.then(function(miJson) {
+  var objectURL = URL.createObjectURL(miBlob);
+  miImagen.src = objectURL;
+});
