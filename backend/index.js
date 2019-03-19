5ca80617d87278206ba2f0638f4a786cec064b1c
Eduardo Chavez <jose_lalo_13@hotmail.com>
2019-03-18 23:48:13 -0700

    CORS

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const morgan = require('morgan');
 
 
 //console.log(process.env.NODE_ENV)
 //INITIALIZATIONS
 const app = express();
-app.use(cors())
+//app.use(cors())
 require('./database');
 
 //SETTINGS
@@ -18,6 +18,14 @@ const port = process.env.PORT || 3000;
 
 
 //MIDDLEWARES
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.use(morgan('dev'));
 /*
