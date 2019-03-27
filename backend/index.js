3fe34ff5ed717eb9a7a7cd713afb7225da062ef2
Eduardo Chavez <jose_lalo_13@hotmail.com>
2019-03-26 21:48:16 -0700

    CORS IMPORT

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const morgan = require('morgan');
 
 
